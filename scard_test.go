a46bcd397c5e20f376e081c1544b7c0aa46ac1a4
Michael Gehring <mg@ebfe.org>
2013-03-10 15:27:38 +0100

    test GetAttrib

---
+++ scard_test.go
@@ -0,0 +1,78 @@
+package scard
+
+import (
+	"testing"
+)
+
+// FIXME: all tests assume pcscd is running, and there is a single reader with a
+// card present
+
+type testCard struct {
+	ctx  *Context
+	card *Card
+}
+
+func setup(t *testing.T) *testCard {
+	var ctx *Context
+	var card *Card
+
+	ctx, err := EstablishContext()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	defer func() {
+		if card == nil {
+			ctx.Release()
+		}
+	}()
+
+	readers, err := ctx.ListReaders()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	if len(readers) == 0 {
+		t.Logf("No readers")
+		t.FailNow()
+	}
+
+	card, err = ctx.Connect(readers[0], SHARE_EXCLUSIVE, PROTOCOL_ANY)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	return &testCard{ctx: ctx, card: card}
+}
+
+func teardown(c *testCard) {
+	if c.card != nil {
+		c.card.Disconnect(LEAVE_CARD)
+	}
+
+	if c.ctx != nil {
+		c.ctx.Release()
+	}
+}
+
+func TestGetAttrib(t *testing.T) {
+	c := setup(t)
+	defer teardown(c)
+
+	atr, err := c.card.GetAttrib(ATTR_ATR_STRING)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Logf("ATTR_ATR_STRING: % x\n", atr)
+	}
+
+	vendor, err := c.card.GetAttrib(ATTR_VENDOR_NAME)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Logf("ATTR_VENDOR_NAME: %s [% x]\n", string(vendor), vendor)
+	}
+}
